547e359cf5830c175220eed22f571efe538c646b
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-06 17:48:23 +0900

    chapter01 Decomposing and Redistributing the Statement Method #6 Extracting Frequent Renter Points

--- chapter01/rental.rb
+++ chapter01/rental.rb
@@ -4,7 +4,7 @@ class Rental
   def initialize(movie, days_rented)
     @movie, @days_rented = movie, days_rented
   end
-  
+
   def charge
     result = 0
     case movie.price_code
@@ -18,5 +18,9 @@ class Rental
       result += (days_rented -2) * 1.5 if days_rented > 3
     end
     result
+  end
+
+  def frequent_renter_points
+    movie.price_code == Movie.NEW_RELEASE && days_rented > 1 ? 2 : 1
   end
 end
