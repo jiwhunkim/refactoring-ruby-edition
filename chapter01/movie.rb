dfe29f8a85140f1e21cf356fbca82670e0f51c18
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-09 13:27:19 +0900

    chapter01 Replacing the Conditional Logic on Price Code with Polymorphism #2 add frequent_renter_points method into movie and then frequent_renter_points on rental to use the new method

--- chapter01/movie.rb
+++ chapter01/movie.rb
@@ -20,8 +20,12 @@ class Movie
       result += days_rented * 3
     when Movie::CHILDRENS
       result += 1.5
-      result += (days_rented -2) * 1.5 if days_rented > 3
+      result += (days_rented -3) * 1.5 if days_rented > 3
     end
     result
+  end
+
+  def frequent_renter_points
+    movie.price_code == Movie.NEW_RELEASE && days_rented > 1 ? 2 : 1
   end
 end
