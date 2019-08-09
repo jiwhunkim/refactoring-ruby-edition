8709df649f18f926492dcc77d3f07a362215bdab
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-09 10:55:58 +0900

    chapter01 Decomposing and Redistributing the Statement Method #9 create method total_frequent_renter_points total_frequent_renter_points apply Collection Closure Method and using inject.

--- chapter01/customer.rb
+++ chapter01/customer.rb
@@ -11,27 +11,22 @@ class Customer
   end
 
   def statement
-    frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-
-      # add frequen renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-        frequent_renter_points += 1
-      end
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
-
       # add footer lines
       result += "Amount owed is #{total_charge}\n"
-      result += "You earned #{frequent_renter_points} frequent renter points"
+      result += "You earned #{total_frequent_renter_points} frequent renter points"
       result
     end
   end
 
   def total_charge
     @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
   end
 end
