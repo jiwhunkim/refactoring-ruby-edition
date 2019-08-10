6bfc84734fc29bf2ea0b83f8b38a4ce77dc53382
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-10 18:25:38 +0900

    chapter04 The Test::Unit Testing Framework #1 first writing test file read compare character

---
+++ chapter04/file_test.rb
@@ -0,0 +1,8 @@
+require "test/unit"
+
+class RunFileTest < Test::Unit::TestCase
+  def test_read_4th_character
+    contents = File.read('data.txt')
+    assert_equal 'd', contents[3, 1]
+  end
+end
