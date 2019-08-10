6bfb37c475a3500aa4a0c24f9faa07df9428ee96
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-10 18:28:05 +0900

    chapter04 The Test::Unit Testing Framework #2 add fail test case read contents

--- chapter04/file_test.rb
+++ chapter04/file_test.rb
@@ -5,4 +5,9 @@ class RunFileTest < Test::Unit::TestCase
     contents = File.read('data.txt')
     assert_equal 'd', contents[3, 1]
   end
+
+  def test_read_4th_content_is_2
+    contents = File.read('data.txt')
+    assert_equal '2', contents[3, 1]
+  end
 end
