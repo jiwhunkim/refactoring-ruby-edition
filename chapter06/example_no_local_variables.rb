717d70df805e094f871064ded7b191f34e1a73b7
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-21 21:39:26 +0900

    chapter06 Extract Method #3 Example: No Local Variables

---
+++ chapter06/example_no_local_variables.rb
@@ -0,0 +1,21 @@
+class ExampleNoLocalVariables
+  attr_reader :orders
+
+  def print_owing
+    outstanding = 0.0
+
+    # print banner
+    puts "*************************"
+    puts "***** Customer Owes *****"
+    puts "*************************"
+
+    # calculate outstanding
+    @orders.each do |order|
+      outstanding += order.amount
+    end
+
+    # print details
+    puts "name: #{@name}"
+    puts "amount: #{outstanding}"
+  end
+end
