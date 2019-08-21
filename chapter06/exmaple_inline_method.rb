9d4edf101803f6fb303ad0a4cd50fb0007d7ca56
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-21 22:11:52 +0900

    chapter06 Inline Method #1 Example: Inline Method

---
+++ chapter06/exmaple_inline_method.rb
@@ -0,0 +1,11 @@
+class ExmapleInlineMethod
+  attr_reader :number_of_late_deliveries
+
+  def get_rating
+    more_than_five_late_deliveries ? 2 : 1
+  end
+
+  def more_than_five_late_deliveries
+    @number_of_late_deliveries > 5
+  end
+end
