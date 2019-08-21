aaf6104f23d85dd0537ba1da2df367008f1f5ed1
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-21 21:45:05 +0900

    chapter06 Extract Method #5 Example: Using Local Variables

---
+++ chapter06/example_using_local_variables.rb
@@ -0,0 +1,25 @@
+class ExampleUsingLocalVariables
+  attr_reader :orders
+
+  def print_owing
+    outstanding = 0.0
+
+    print_banner
+
+    # calculate outstanding
+    @orders.each do |order|
+      outstanding += order.amount
+    end
+
+    # print details
+    puts "name: #{@name}"
+    puts "amount: #{outstanding}"
+  end
+
+  def print_banner
+    # print banner
+    puts "*************************"
+    puts "***** Customer Owes *****"
+    puts "*************************"
+  end
+end
