6f6fc88e3f4b8121caf8485521da7c89116c9dd7
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-23 20:40:28 +0900

    chapter06 Replace Temp with Query #5 Example: Replace Temp with Query 2 replace the first reference to the temp

--- chapter06/example_replace_temp_with_query2.rb
+++ chapter06/example_replace_temp_with_query2.rb
@@ -3,7 +3,7 @@ class ExampleReplaceTempWithQuery2
 
   def price
     a_base_price = base_price
-    if a_base_price > 1000
+    if base_price > 1000
       discount_factor = 0.95
     else
       discount_factor = 0.98
