8c7b68833678cf9773b65ce2d5e43ba25151618a
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-23 21:51:28 +0900

    chapter06 Introduce Explaining Variable #3 example introduce explaining variable 2

---
+++ chapter06/example_introduce_explaining_variable.rb
@@ -0,0 +1,8 @@
+class ExampleIntroduceExplainingVariable
+  def price
+    # price is base price - quantity discount + shipping
+    return @quantity * @item_price -
+        [0, @quantity - 500].max * @item_price * 0.05 +
+        [@quantity * @item_price * 0.1, 100.0].min
+  end
+end
