e68e6351a22272139442431bda8f9c94cb4a87e9
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-23 22:27:10 +0900

    chapter06 Introduce Explaining Variable #9 example with extract method 1 extract a method for the base price

--- chapter06/example_with_extract_method.rb
+++ chapter06/example_with_extract_method.rb
@@ -1,7 +1,11 @@
 class ExampleWithExtractMethod
   def price
-    return @quantity * @item_price -
+    return base_price -
         [0, @quantity - 500].max * @item_price * 0.05 +
-        [@quantity * @item_price * 0.1, 100.0].min
+        [base_price * 0.1, 100.0].min
+  end
+  
+  def base_price
+    @quantity * @item_price
   end
 end
