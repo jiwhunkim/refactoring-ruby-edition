d4d522959e36d058d49996c59ece90b32d354acd
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-23 21:09:26 +0900

    chapter06 Replace Temp with Chain #4 Example Replace Temp with Chain 2 creating a Method Chained solution is to create a method that creates the Select instance and adds an option

--- chapter06/example_replace_temp_with_chain_select.rb
+++ chapter06/example_replace_temp_with_chain_select.rb
@@ -1,4 +1,10 @@
 class ExampleReplaceTempWithChainSelect
+  def self.with_option(option)
+    select = self.new
+    select.options << option
+    select
+  end
+
   def options
     @options ||= []
   end
@@ -8,8 +14,7 @@ class ExampleReplaceTempWithChainSelect
   end
 end
 
-select = ExampleReplaceTempWithChainSelect.new
-select.add_option(1999)
+select = ExampleReplaceTempWithChainSelect.with_option(1999)
 select.add_option(2000)
 select.add_option(2001)
 select.add_option(2002)
