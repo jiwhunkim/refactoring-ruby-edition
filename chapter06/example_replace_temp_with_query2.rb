12d7452e527b09894520cdc55f1f5a6fc07e84bc
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-23 20:41:31 +0900

    chapter06 Replace Temp with Query #6 Example: Replace Temp with Query 2 remove the temp assignment

--- chapter06/example_replace_temp_with_query2.rb
+++ chapter06/example_replace_temp_with_query2.rb
@@ -2,13 +2,12 @@ class ExampleReplaceTempWithQuery2
   attr_reader :quantity, :item_price
 
   def price
-    a_base_price = base_price
     if base_price > 1000
       discount_factor = 0.95
     else
       discount_factor = 0.98
     end
-    a_base_price * discount_factor
+    base_price * discount_factor
   end
 
   def base_price
