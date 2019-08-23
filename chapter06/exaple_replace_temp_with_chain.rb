cd659eb0072b4029ee21a5356a5e100d1a3b4256
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-23 20:50:59 +0900

    chapter06 Replace Temp with Chain #1 Example Replace Temp with Chain

---
+++ chapter06/exaple_replace_temp_with_chain.rb
@@ -0,0 +1,8 @@
+class ExapleReplaceTempWithChain
+  def exmaple_chain
+    mock = Mock.new
+    expectation = mock.expects(:a_method_name)
+    expectation.with("arguments")
+    expectation.returns([1, :array])
+  end
+end
