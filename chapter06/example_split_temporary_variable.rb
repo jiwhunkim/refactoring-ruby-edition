fccc55ddd973d1d42de4472252aeff9bc74fb2b9
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-25 19:15:56 +0900

    chapter06 Split Temporary Variable #5 example with split temporary variable second assignment of the temp

--- chapter06/example_split_temporary_variable.rb
+++ chapter06/example_split_temporary_variable.rb
@@ -6,8 +6,8 @@ class ExampleSplitTemporaryVariable
     secondary_time = time - @delay
     if (secondary_time > 0)
       primary_vel = primary_acc * @delay
-      acc = (@prmary_force + @secondary_force) / @mass
-      result += primary_vel * secondary_time + 5 * acc * secondary_time * secondary_time
+      secondary_acc = (@prmary_force + @secondary_force) / @mass
+      result += primary_vel * secondary_time + 5 * secondary_acc * secondary_time * secondary_time
     end
     result
   end
