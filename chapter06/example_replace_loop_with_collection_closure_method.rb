6f0d45b5c2416bd72e4a7e7c79fcad72a3464cf6
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-31 17:49:04 +0900

    chapter06 Replace Loop with Collection Closure Method #6 Example Replace Loop with Collection Closure Method manager office refactor

--- chapter06/example_replace_loop_with_collection_closure_method.rb
+++ chapter06/example_replace_loop_with_collection_closure_method.rb
@@ -1,19 +1,16 @@
 class ExampleReplaceLoopWithCollectionClosureMethod
   def employee_managers
-    managers = employees.select {|e| e.manager?}
+    managers = employees.select { |e| e.manager? }
     managers
   end
 
   def employee_offices
-    offices = employees.collect {|e| e.office}
+    offices = employees.collect { |e| e.office }
     offices
   end
 
   def manager_office
-    manager_offices = []
-    employees.each do |e|
-      manager_offices << e.office if e.manager?
-    end
+    manager_offices = employees.select { |e| e.manager? }.collect { |e| e.office }
     manager_offices
   end
 end
