8689e8a918ecd70ed362c1cb4b7c40d55e922133
Jiwhun Kim <jiwhun.kim@outlook.com>
2019-08-31 18:01:45 +0900

    chapter06 Extract Surrounding Method #2 Explain Extract Surrounding Method refactor

--- chapter06/extract_surrounding_method.rb
+++ chapter06/extract_surrounding_method.rb
@@ -1,13 +1,19 @@
 class ExtractSurroundingMethod
   def charge(amount, credit_card_number)
-    begin
-      connection = CreditCardServer.connect(...)
+    connect do |connection|
       connection.send(amount, credit_card_number)
-    rescue IOError => e
-      Logger.log "Could not submit order #{@order_number} to the server: #{e}"
-      return nil
-    ensure
-      connection.close
+    end
+
+    def connect
+      begin
+        connection = CreditCardServer.connect(...)
+        yield connection
+      rescue IOError => e
+        Logger.log "Could not submit order #{@order_number} to the server: #{e}"
+        return nil
+      ensure
+        connection.close
+      end
     end
   end
 end
